32b89462fd848d3c89a2ffd39ccaf45a316d808c
Luca <lucacm@al.insper.edu.br>
2023-03-10 12:19:57 +0000

    aula busca local

--- busca-local/mochila.cpp
+++ busca-local/mochila.cpp
@@ -1,8 +1,14 @@
 // esse é o código do mais leve - alterar para aleatorização e depois busca local
+// para rodar passar o in.txt (./mochila < in1.txt)
+// objetivos: primeiro da um shuffle na entrada, depois realiza uma rodada aleatoria de adição.
+// com os itens sobrados aplica-se uma condição (se cabe) de entrada e faz uma segunda rodada de adição.
+// 1 amostra é a quantidade de vez que roda o código
 
+#include<chrono>
 #include<iostream>
 #include<vector>
 #include<algorithm>
+#include<random>
 using namespace std;
 
 struct item {
@@ -11,16 +17,12 @@ struct item {
     double valor;
 };
 
-bool my_compare(item a, item b){
-    return a.peso < b.peso; // ordenando pelo mais leve
-}
-
 int main() {
     int n = 0;
     int W = 0;
     vector<item> mochila;
     vector<item> items;
-    cin >> n >> W;
+    cin >> n >> W; // lendo número de items e peso
     items.reserve(n);
     double peso, valor;
     for(int i = 0; i < n; i++){
@@ -28,23 +30,32 @@ int main() {
         cin >> valor;
         items.push_back({i, peso, valor});
     }
-    //ordenar
-    sort(items.begin(), items.end(), my_compare);
-    peso = 0;
-    valor = 0;
-    for(auto& el : items){
-        if(el.peso + peso <= W){
-            mochila.push_back(el);
-            peso += el.peso;
-            valor += el.valor;
-        }
-    }
-    //ordenando para imprimir
-    sort(mochila.begin(), mochila.end(), [](auto& i, auto&j){return i.id < j.id;});
-    cout << peso << " " << valor << " 0" << endl;
-    for(auto& el: mochila){
-        cout << el.id << " ";
-    }
+
+    //imprimir
+    // cout << "itens lidos: (id, peso, valor)" << endl;
+    // for(auto& el: items){
+    //     cout << el.id << "\t" << el.peso << "\t" << el.valor << endl;
+    // }
+
+    // gera probabilidade de 50%
+    unsigned seed = chrono::system_clock::now().time_since_epoch().count();
+    default_random_engine generator (seed);
+    uniform_int_distribution<int> distribution(0,1);
+    cout << distribution(generator) << endl; // gera número
+    
+    // aleatorizar vetor
+    // random_shuffle(vetor.begin(), vetor.end())
+
+    // aleatorizar mochila
+    // peso = 0;
+    // valor = 0;
+    // for(auto& el : items){
+    //     if(){
+    //         mochila.push_back(el);
+    //         peso += el.peso;
+    //         valor += el.valor;
+    //     }
+    // }
 
     return 0;
 }
